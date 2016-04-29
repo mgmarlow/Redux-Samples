bad997f0db9d648c7fc04aa0c4893e62d8ca1702
Graham Marlow <mgmarlow@gmail.com>
2016-04-29 16:19:19 -0700

    Edit webpack to use new src directory

--- examples/counter/webpack.config.js
+++ examples/counter/webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  context: path.join(__dirname, ''),
+  context: path.join(__dirname, 'src'),
   entry: './index.js',
   module: {
     loaders: [
