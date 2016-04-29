b86ddf35eae58ec4dfec21f6c58adcb754995723
Graham Marlow <mgmarlow@gmail.com>
2016-04-29 16:06:05 -0700

    update readme

---
+++ examples/preventMutations/objectMutations.js
@@ -0,0 +1,30 @@
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
+
+const toggleTodo = (todo) => {
+  // Note: Object.assign requires polyfill
+  return Object.assign({}, todo, {
+    completed: !todo.completed
+  });
+};
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
+
+  deepFreeze(todoBefore);
+
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter);
+};
+
+testToggleTodo();
